f4da6ddd7a09ca0de954b1823e0e1f8bfd359e4d
Srdjan Djoric <srdjan.djoric@bankom.rs>
2020-11-02 12:39:27 +0100

    nista

--- Bankom/BankomMDI.cs
+++ Bankom/BankomMDI.cs
@@ -4029,15 +4029,14 @@ private void MenuItemClickHandler(object sender, EventArgs e)
             // return dt1.Select("ID_ArtikliStablo ='" + strArtikliID + "'")[0][0].ToString();
 
             DataBaseBroker db = new DataBaseBroker();
-            SqlConnection conn = new SqlConnection(connectionString);
-            conn.Open();
+         
             string param0 = pomId;
-            string param1 = "";
-            param1 = pomStablo;
+            string param1 = strArtikliID;
+           // param1 = pomStablo;
             string upit = "Select @param0 from " + pomStablo + " where  NazivJavni =@param1"  ;
             //SqlCommand cmd = new SqlCommand(upit, conn);
            
-            DataTable rez = db.ParamsQueryDT(upit,param0, strArtikliID);
+            DataTable rez = db.ParamsQueryDT(upit,param0, param1);
             //conn.Close();
             string s = rez.Rows[0][0].ToString();
             return s;
