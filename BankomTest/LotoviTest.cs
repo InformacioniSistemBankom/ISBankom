239a1d7cb61c645d2f9d3844338e4341d3269dcd
mspaic96 <57438931+mspaic96@users.noreply.github.com>
2020-11-26 10:05:41 +0100

    Test Project Add!

---
+++ BankomTest/LotoviTest.cs
@@ -0,0 +1,15 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Bankom;
+
+namespace BankomTest
+{
+    [TestClass]
+    public class LotoviTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
